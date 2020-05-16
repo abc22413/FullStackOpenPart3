efc6023ad38909c67f8203ba62bedd12059f1864
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2020-05-17 00:27:25 +0800

    finished 3a tasks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
 let persons = [
   {
@@ -26,8 +26,72 @@ let persons = [
   }
 ]
 
+//collectionView of all persons
 app.get("/api/persons", (request, response) => {
   response.json(persons)
+})
+
+//InfoView
+app.get("/info", (request, response) => {
+  const time = new Date()
+  response.send(`
+  <p>Phonebook has info for ${persons.length} ${persons.length !== 1 ? "people" : "person"}</p>
+  <p>${time}</p>
+  `)
+})
+
+//Retrieve singular resource
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if(person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+//Delete singular resoucrce
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+//add new person
+app.get("/lol", (request, response) => {
+  const genID = () => {
+    let newID = Math.floor(Math.random()*Number.MAX_VALUE)
+    while (persons.map(p => p.id).includes(newID)) {
+      newID = Math.floor(Math.random()*Number.MAX_VALUE)
+    }
+    return newID
+  }
+
+  const body = request.body
+
+  //Name or number missing
+  if (!body.name || !body.number) {
+    response.status(400).json({
+      error: "Name and/or number missing"
+    })
+  }
+  //Name aready inside
+  else if (persons.map(p => p.name).includes(body.name)) {
+    response.status(400).json({
+      error: "Name already exists"
+    })
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: genID()
+  }
+  persons = persons.concat(newPerson)
+
+  response.json(newPerson)
 })
 
 const PORT = 3001
