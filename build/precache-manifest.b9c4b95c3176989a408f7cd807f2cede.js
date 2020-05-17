9b24b9e5d419da1bdabc63a62236c3c4d8578603
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2020-05-17 14:55:44 +0800

    ready to deploy SPA

---
+++ build/precache-manifest.b9c4b95c3176989a408f7cd807f2cede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d831fe8424c69009b082b40629ff836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da9c83f8379307f89c63",
+    "url": "/static/js/2.6de0d325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6de0d325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0f88a735fe5744bd8d",
+    "url": "/static/js/main.bd3155fa.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
